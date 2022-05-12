0da5611d3d41d1782ac9d4bedfa976f05315bd6a
kenjugs <ramarkrusher@gmail.com>
2022-05-11 23:28:00 -0700

    added solutions for problem 11

---
+++ csharp/11_-_Container_with_Most_Water.cs
@@ -0,0 +1,28 @@
+/**
+ * 11. Container with Most Water - Medium
+ * You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ * Return the maximum amount of water a container can store.
+ * Notice that you may not slant the container.
+ */
+
+public class Solution {
+    public int MaxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.Length - 1;
+        
+        while (left < right) {
+            int area = (right - left) * Math.Min(height[left], height[right]);
+            maxArea = Math.Max(maxArea, area);
+            
+            if (height[left] < height[right]) {
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        
+        return maxArea;
+    }
+}
