f262466810c84d68822e24d22efcf1f2ea8fe14c
kenjugs <ramarkrusher@gmail.com>
2022-05-19 12:46:28 -0700

    added solutions for problem 55

---
+++ csharp/55_-_Jump_Game.cs
@@ -0,0 +1,21 @@
+/**
+ * 55. Jump Game - Medium
+ * You are given an integer array nums. You are initially positioned at the
+ * array's first index, and each element in the array represents your maximum
+ * jump length at that position.
+ * Return true if you can reach the last index, or false otherwise.
+ */
+
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int lastPos = nums.Length - 1;
+        
+        for (int i = nums.Length - 2; i >= 0; --i) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        
+        return lastPos == 0;
+    }
+}
---
+++ javascript/55_-_Jump_Game.js
@@ -0,0 +1,23 @@
+/**
+ * 55. Jump Game - Medium
+ * You are given an integer array nums. You are initially positioned at the
+ * array's first index, and each element in the array represents your maximum
+ * jump length at that position.
+ * Return true if you can reach the last index, or false otherwise.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let lastPos = nums.length - 1;
+    
+    for (let i = nums.length - 2; i >= 0; --i) {
+        if (i + nums[i] >= lastPos) {
+            lastPos = i;
+        }
+    }
+    
+    return lastPos === 0;
+};
